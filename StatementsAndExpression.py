f1150f50e2a09cdc16594a8c2a84d961d024f542
Ancalim <76219800+Ancalim@users.noreply.github.com>
2020-12-21 16:11:30 +0300

    Утверждения и выражения

--- StatementsAndExpression.py
+++ StatementsAndExpression.py
@@ -13,3 +13,16 @@ print (2*len("hello")+len("goodbye"))
 x=2
 y=1
 print(sub(square(y),square(x)))  #Функция не работает.
+
+
+#Подробно о том, как проводит работу питон
+
+#x=5
+#y=7
+
+#add(square(y),square(x))
+#add(square(7),square(x))
+#add(square(7),square(5))
+#add(49,square(5))
+#add(49,25)
+#add(74)
